d691348cb500d62fd2a2efb8cd5154f14539fa5d
chris <Mileschris466@gmail.com>
2024-09-29 03:34:59 +0300

    arrays commikt

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const divide = function() {
+    const a = 2000;
+    const b = 100;
+
+    return a / b;
+}
+
+const add = function(){
+    const a = 3;
+    const b = 4;
+
+    return a + b;
+}
+
+const square = function() {
+    const x = 2;
+
+    return x * x;
+}
